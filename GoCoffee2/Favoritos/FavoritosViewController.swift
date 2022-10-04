8b512e4597a57a6a3d29b67b7853c7b097c8e60c
David Augusto <daviaugusto222@gmail.com>
2022-10-04 13:30:26 -0300

    add favorite tableview

---
+++ GoCoffee2/Favoritos/FavoritosViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FavoritosViewController.swift
+//  GoCoffee2
+//
+//  Created by Admin on 04/10/22.
+//
+
+import UIKit
+
+class FavoritosViewController: UIViewController {
+    @IBOutlet weak var favoritostableview: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
